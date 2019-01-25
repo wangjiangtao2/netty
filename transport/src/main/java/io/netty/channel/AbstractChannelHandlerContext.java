2f6bc8ccd01a6fdf5d5a4efb8a393cd6787f1adf
Norman Maurer <norman_maurer@apple.com>
2019-01-25 08:16:57 +0100

    Address comments

--- transport/src/main/java/io/netty/channel/AbstractChannelHandlerContext.java
+++ transport/src/main/java/io/netty/channel/AbstractChannelHandlerContext.java
@@ -24,7 +24,6 @@ import io.netty.util.ReferenceCountUtil;
 import io.netty.util.ResourceLeakHint;
 import io.netty.util.concurrent.EventExecutor;
 import io.netty.util.concurrent.FastThreadLocal;
-import io.netty.util.concurrent.OrderedEventExecutor;
 import io.netty.util.internal.PlatformDependent;
 import io.netty.util.internal.PromiseNotificationUtil;
 import io.netty.util.internal.ThrowableUtil;
@@ -104,16 +103,14 @@ abstract class AbstractChannelHandlerContext extends DefaultAttributeMap
     private final DefaultChannelPipeline pipeline;
     private final String name;
 
-    final EventExecutor executor;
     private ChannelFuture succeededFuture;
 
     private volatile int handlerState = INIT;
 
-    AbstractChannelHandlerContext(DefaultChannelPipeline pipeline, OrderedEventExecutor executor, String name,
+    AbstractChannelHandlerContext(DefaultChannelPipeline pipeline, String name,
                                   Class<? extends ChannelHandler> handlerClass) {
         this.name = ObjectUtil.checkNotNull(name, "name");
         this.pipeline = pipeline;
-        this.executor = executor;
         this.executionMask = mask(handlerClass);
     }
 
@@ -233,11 +230,6 @@ abstract class AbstractChannelHandlerContext extends DefaultAttributeMap
     @Override
     public ByteBufAllocator alloc() {
         return channel().config().getAllocator();
-    }
-
-    @Override
-    public EventExecutor executor() {
-        return executor;
     }
 
     @Override
@@ -566,7 +558,7 @@ abstract class AbstractChannelHandlerContext extends DefaultAttributeMap
         if (executor.inEventLoop()) {
             findAndInvokeConnect(remoteAddress, localAddress, promise);
         } else {
-            safeExecute(executor, () ->findAndInvokeConnect(remoteAddress, localAddress, promise), promise, null);
+            safeExecute(executor, () -> findAndInvokeConnect(remoteAddress, localAddress, promise), promise, null);
         }
         return promise;
     }
@@ -636,7 +628,7 @@ abstract class AbstractChannelHandlerContext extends DefaultAttributeMap
         if (executor.inEventLoop()) {
             findAndInvokeClose(promise);
         } else {
-            executor.execute(() -> findAndInvokeClose(promise));
+            safeExecute(executor, () -> findAndInvokeClose(promise), promise, null);
         }
         return promise;
     }
@@ -801,9 +793,10 @@ abstract class AbstractChannelHandlerContext extends DefaultAttributeMap
             throw e;
         }
 
-        final AbstractChannelHandlerContext next = findContextOutbound(flush ? (MASK_WRITE | MASK_FLUSH) : MASK_WRITE);
         EventExecutor executor = executor();
         if (executor.inEventLoop()) {
+            final AbstractChannelHandlerContext next = findContextOutbound(flush ?
+                    (MASK_WRITE | MASK_FLUSH) : MASK_WRITE);
             if (flush) {
                 next.invokeWriteAndFlush(msg, promise);
             } else {
